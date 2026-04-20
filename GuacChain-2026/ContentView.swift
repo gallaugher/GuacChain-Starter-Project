840eed35679384ac9ec7f820fe13681d722aa37a
gallaugher <john.gallaugher@bc.edu>
2026-04-20 17:53:35 -0400

    Initial Commit

---
+++ GuacChain-2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuacChain-2026
+//
+//  Created by John Gallaugher on 4/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuacChain-2026/GuacChain_2026App.swift
@@ -0,0 +1,17 @@
+//
+//  GuacChain_2026App.swift
+//  GuacChain-2026
+//
+//  Created by John Gallaugher on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GuacChain_2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
